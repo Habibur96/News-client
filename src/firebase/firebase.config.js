ca1948a16d41c3e64f78d33c6455f94cb08df0dc
Habibur Rahman Nayan <habiburrahmannayan66@gmail.com>
2023-08-10 22:07:27 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,15 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log("Environment variable : ", import.meta.env.VITE_apikey);
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBJPmEEG69g0kMZCZU7v-1YtRmr7z-1Lzg",
-  authDomain: "the-news-dragon-client-5d883.firebaseapp.com",
-  projectId: "the-news-dragon-client-5d883",
-  storageBucket: "the-news-dragon-client-5d883.appspot.com",
-  messagingSenderId: "896503869959",
-  appId: "1:896503869959:web:7f0aaae73a786d377ef0a2",
+  apiKey: "import.meta.env.VITE_APIKE ",
+  authDomain: "import.meta.env.VITE_AUTHDOMAIN ",
+  projectId: "import.meta.env.VITE_PROJECTID ",
+  storageBucket: "import.meta.env.VITE_STORAGEBUCKET ",
+  messagingSenderId: "import.meta.env.VITE_MESSAGINGSENDERID ",
+  appId: "import.meta.env.VITE_APPID ",
 };
 
 // Initialize Firebase
